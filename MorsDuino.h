1b368b5906eea3a861b8c048d8393acbd4ea5228
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-11 01:06:34 +0300

    fix string

--- MorsDuino.h
+++ MorsDuino.h
@@ -1,19 +1,19 @@
 #ifndef MORSDUINO_H
 #define MORSDUINO_H
 
-#include <string>
 #include "Arduino.h"
 
 class MorsDuino {
     public:
         MorsDuino(int pin = 13);
-        void displayText(std::string text);
+        void displayText(String text);
         void displayInt(int number);
         void displayChar(char character);
     private:
         int _pin = 13;
         void _dot(int count = 1);
         void _dash(int count = 1);
+        void _delaySeparator(int type);
 };
 
 #endif // MORSDUINO_H
