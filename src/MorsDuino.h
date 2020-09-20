1c40183406212b6adaa8f3ce4c5e46c14647d2d8
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-20 18:15:26 +0300

    add examples

--- src/MorsDuino.h
+++ src/MorsDuino.h
@@ -3,42 +3,39 @@
 
 #include "Arduino.h"
 
-class MorsDuino
-{
-public:
-    class Led
-    {
-    public:
-        Led(int pin);
-        void displayString(String text);
-        void displayChar(char character);
-        void displayInt(int number);
+class MorsDuino {
+ public:
+  class Led {
+   public:
+    Led(int pin);
+    void displayString(String text);
+    void displayChar(char character);
+    void displayInt(int number);
 
-    private:
-        int _pin;
-        int _letterSeparatorDelay;
-        int _wordSeparatorDelay;
-        void _dot(int count);
-        void _dash(int count);
-        void _delaySeparator(int type);
-    };
-    class RgbLed
-    {
-    public:
-        RgbLed(int red, int green, int blue);
-        void displayString(char color, String text);
-        void displayInt(char color, int number);
-        void displayChar(char color, char character);
+   private:
+    int _pin;
+    int _letterSeparatorDelay;
+    int _wordSeparatorDelay;
+    void _dot(int count);
+    void _dash(int count);
+    void _delaySeparator(int type);
+  };
+  class RgbLed {
+   public:
+    RgbLed(int red, int green, int blue);
+    void displayString(char color, String text);
+    void displayInt(char color, int number);
+    void displayChar(char color, char character);
 
-    private:
-        int _redPin;
-        int _greenPin;
-        int _letterSeparatorDelay;
-        int _wordSeparatorDelay;
-        void _dot(int count);
-        void _dash(int count);
-        void _delaySeparator(int type);
-    };
+   private:
+    int _redPin;
+    int _greenPin;
+    int _letterSeparatorDelay;
+    int _wordSeparatorDelay;
+    void _dot(int count);
+    void _dash(int count);
+    void _delaySeparator(int type);
+  };
 };
 
-#endif // MORSDUINO_H
+#endif  // MORSDUINO_H
