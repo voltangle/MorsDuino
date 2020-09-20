56ce2053b6a31a800aa82d4a37782c4137dbfaba
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-20 17:04:06 +0300

    update architecture

--- src/MorsDuino.h
+++ src/MorsDuino.h
@@ -3,21 +3,42 @@
 
 #include "Arduino.h"
 
-class MorsDuinoLed
+class MorsDuino
 {
 public:
-    MorsDuinoLed(int pin);
-    void displayString(String text);
-    void displayChar(char character);
-    void displayInt(int number);
+    class Led
+    {
+    public:
+        Led(int pin);
+        void displayString(String text);
+        void displayChar(char character);
+        void displayInt(int number);
 
-private:
-    int _pin;
-    int _letterSeparatorDelay;
-    int _wordSeparatorDelay;
-    void _dot(int count = 1);
-    void _dash(int count = 1);
-    void _delaySeparator(int type);
+    private:
+        int _pin;
+        int _letterSeparatorDelay;
+        int _wordSeparatorDelay;
+        void _dot(int count);
+        void _dash(int count);
+        void _delaySeparator(int type);
+    };
+    class RgbLed
+    {
+    public:
+        RgbLed(int red, int green, int blue);
+        void displayString(char color, String text);
+        void displayInt(char color, int number);
+        void displayChar(char color, char character);
+
+    private:
+        int _redPin;
+        int _greenPin;
+        int _letterSeparatorDelay;
+        int _wordSeparatorDelay;
+        void _dot(int count);
+        void _dash(int count);
+        void _delaySeparator(int type);
+    };
 };
 
 #endif // MORSDUINO_H
